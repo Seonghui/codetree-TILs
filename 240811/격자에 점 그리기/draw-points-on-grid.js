b71c9dd2b0281bd964c7438024b252c9ed2a8634
Stella Yu <stellashyou@gmail.com>
2024-08-11 20:14:34 +0900

    Title: 격자에 점 그리기, Time: 211ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240811/격자에 점 그리기/draw-points-on-grid.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split("\n");
+let [n, m] = input[0].split(' ').map(Number);
+
+let arr2d = Array(n).fill(0).map(() => Array(n).fill(0));
+
+let num = 1
+for (let i = 1; i <= m; i++) {
+    const [r, c] = input[i].split(' ').map(Number);
+    arr2d[r - 1][c - 1] = num;
+    num++
+}
+
+for (let row of arr2d) {
+    console.log(row.join(' '))
+}
