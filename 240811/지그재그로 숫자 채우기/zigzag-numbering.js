f2f34a6149cbe6f4e56af654f44f2313de53a0ae
Stella Yu <stellashyou@gmail.com>
2024-08-11 18:10:22 +0900

    Title: 지그재그로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240811/지그재그로 숫자 채우기/zigzag-numbering.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().split(" ");
+let n = Number(arr[0]);
+let m = Number(arr[1]);
+
+let arr2d = Array(n).fill(0).map(() => Array(m).fill(0));
+
+let num = 0
+for (let i = 0; i < m; i++) {
+    if (i % 2 === 0) {
+        for (let j = 0; j < n; j++) {
+            arr2d[j][i] = num;
+            num++
+        }    
+    } else {
+        for (let j = m + 1; j >= 0; j--) {
+            arr2d[j][i] = num;
+            num++
+        }
+    }
+    
+}
+
+for (let a of arr2d) {
+    console.log(a.join(' '))
+}
