e6260319307ef802efecbcf3e88fffa09423f7cb
Stella Yu <stellashyou@gmail.com>
2024-08-15 17:50:20 +0900

    Title: 가장 짧은 문자열, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240815/가장 짧은 문자열/shortest-string.js
+++ 240815/가장 짧은 문자열/shortest-string.js
@@ -4,11 +4,11 @@ let max = items[0]
 let min = items[0]
 
 for (let item of items) {
-    if (max.length < item.length) {
+    if (max.length <= item.length) {
         max = item
         continue
     }
-    if (min.length > item.length) {
+    if (min.length >= item.length) {
         min = item
         continue
     }
