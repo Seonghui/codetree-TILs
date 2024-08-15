50a837e4e826868ba8cdbe1cc3c35911bc1971f0
Stella Yu <stellashyou@gmail.com>
2024-08-15 17:41:44 +0900

    Title: 더 긴 문자열, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240815/더 긴 문자열/longer-string.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let [a, b] = fs.readFileSync(0).toString().split(" ");
+
+if (a.length > b.length) {
+    console.log(a, a.length)
+} else if (a.length < b.length) {
+    console.log(b, b.length)
+} else {
+    console.log('same')
+}
